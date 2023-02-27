e64f614507464fc0bc0b2cabeb3eb93d41867faf
Anushka Chauhan <anushkachauhan1503@gmail.com>
2023-02-27 13:29:35 +0530

    5 Computing GC Content (with large input)

--- 5-GC.py
+++ 5-GC.py
@@ -2,6 +2,8 @@
 import sys
 import os
 
+data = []
+
 
 class dna_obj:
     def __init__(self, id, str):
@@ -10,22 +12,36 @@ class dna_obj:
         self.gc = (self.str.count('G') + self.str.count('C')) * 100 / len(str)
 
 
-def getMaxGC(lines):
-    # Sort input into suitable data objects
-    data = []
-    for index in range(0, len(lines), 2):
-        dna = dna_obj(lines[index], lines[index+1])
-        data.append(dna)
-
+def getMaxGC():
     # Getting max - https://stackoverflow.com/a/46796164/12302691
     max_dna = max(data, key=lambda node: node.gc)
     return (max_dna.id[1:] + str(max_dna.gc))
 
 
+def sortInput(lines):
+    def isIdString(str):
+        if (str.startswith(">")):
+            return True
+        return False
+
+    for i in range(0, len(lines)):
+        if (isIdString(lines[i])):
+            id = lines[i]
+            str = ""
+            for j in range(i+1, len(lines)):
+                if (isIdString(lines[j])):
+                    break
+                str += lines[j].strip()
+
+            dna = dna_obj(id, str)
+            data.append(dna)
+
+
 def main():
     # Get multiline input
     lines = sys.stdin.readlines()
-    print(getMaxGC(lines))
+    sortInput(lines)
+    print(getMaxGC())
 
 
 if __name__ == '__main__':
