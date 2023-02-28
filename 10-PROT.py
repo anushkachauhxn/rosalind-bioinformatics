d457a517463bf7b6617d408a88bacc267e09a1a2
Anushka Chauhan <anushkachauhan1503@gmail.com>
2023-02-28 13:42:28 +0530

    10 Translating RNA into Protein

---
+++ 10-PROT.py
@@ -0,0 +1,45 @@
+# 10) Translating RNA into Protein
+import sys
+import os
+
+RNA_CODON_TABLE = {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+
+
+def translateToProtein(rna):
+    protein_str = ""
+    for i in range(0, len(rna), 3):
+        codon = rna[i:i+3]
+        symbol = RNA_CODON_TABLE[codon]
+        if (symbol == "Stop"):
+            break
+        protein_str += symbol
+    return protein_str
+
+
+def main():
+    rna = input()
+    print(translateToProtein(rna))
+
+
+if __name__ == '__main__':
+    if not os.environ.get("ONLINE_JUDGE"):
+        sys.stdin = open('./input.txt', 'r')
+        sys.stdout = open('./output.txt', 'w')
+    main()
