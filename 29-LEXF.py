5dbf3c56e125b39d2c36d9075a498f285837d373
Anushka Chauhan <anushkachauhan1503@gmail.com>
2023-03-07 18:13:35 +0530

    29 Enumerating k-mers Lexicographically (better solution)

--- 29-LEXF.py
+++ 29-LEXF.py
@@ -1,21 +1,27 @@
 # 29) Enumerating k-mers Lexicographically
 import os
 import sys
-from itertools import product
+from itertools import combinations_with_replacement
 
 
 def getAllLexograhicalStrings(n, A):
+    combinations = list(combinations_with_replacement(A, n)) + \
+        list(combinations_with_replacement(reversed(A), n))
+
     strings = []
-    for i, j in product(range(len(A)), repeat=n):
-        str = A[i]+A[j]
-        strings.append(str)
+    for x in combinations:
+        str = "".join(x)
+        if (str not in strings):
+            strings.append(str)
+    strings.sort()
+
     return strings
 
 
 def main():
     A = list(input().split(" "))
     n = int(input())
-    [print(x) for x in getAllLexograhicalStrings(n, A)]
+    print(*getAllLexograhicalStrings(n, A), sep='\n')
 
 
 if __name__ == '__main__':
